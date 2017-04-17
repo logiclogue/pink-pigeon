342aec1df23b08f70c97639c8503c0d8982c53e2
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-17 20:48:33 +0100

    Adds indexer of possible letters, learns more Rust, ugly program so far

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,21 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     let input = &args[1];
     let digest = get_hash(input);
+    let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let index = get_random_number(&digest) % 36;
 
-    println!("{}", get_random_number(&digest) % 36);
+
+    println!("{} {} {}", letters, index, get_char_from_index(&letters, index));
+}
+
+fn get_char_from_index(letters: &str, index: i32) -> char {
+    for (i, value) in letters.chars().enumerate() {
+        if i == index as usize {
+            return value;
+        }
+    }
+
+    ' '
 }
 
 fn get_hash(input: &str) -> String {
