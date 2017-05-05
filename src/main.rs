2dd539a98b8d0cd7c2cb8f143055cb748dc4f65e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-05-05 22:29:59 +0100

    Tidies a few things up in the random trait

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ use std::env;
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 use random::Generator;
+use random::SeededGenerator;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let input = &args[1];
     let digest = get_hash(input);
     let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let generator = random::SeededGenerator::new(&digest);
+    let mut generator = SeededGenerator::new(&digest);
     let index = generator.get_in_range(0, 36);
 
     println!("{} {} {}", letters, index, get_char_from_index(&letters, index));
--- src/random.rs
+++ src/random.rs
@@ -2,33 +2,34 @@ use crypto::md5::Md5;
 use crypto::digest::Digest;
 
 pub trait Generator {
-    fn get_in_range(&self, min: i32, max: i32) -> i32;
+    fn get_in_range(&mut self, min: i32, max: i32) -> i32;
 }
 
 pub struct SeededGenerator {
-    seed: String
+    seed: String,
+    hash: Md5
 }
 
 impl SeededGenerator {
     pub fn new(seed: &str) -> SeededGenerator {
         SeededGenerator {
-            seed: seed.to_string()
+            seed: seed.to_string(),
+            hash: Md5::new()
         }
     }
 }
 
 impl Generator for SeededGenerator {
-    fn get_in_range(&self, min: i32, max: i32) -> i32 {
+    fn get_in_range(&mut self, min: i32, max: i32) -> i32 {
         let digest;
         let s: String;
         let num;
-        let mut hash = Md5::new();
         let diff = max - min;
         let result;
 
-        hash.input_str(&self.seed);
+        self.hash.input_str(&self.seed);
 
-        digest = hash.result_str();
+        digest = self.hash.result_str();
 
         s = digest
             .chars()
